711a3a12664433f5e05e5ac3f3074f3411fc966e
semasping <semasping@gmail.com>
2018-01-09 04:04:50 +0100

    add cache settings for laravel tests

---
+++ laravel-5.4/app/Http/Controllers/PostsCacheController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Posts;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
+class PostsCacheController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Cache::rememberForever('posts2', function () {
+            $posts = Posts::with(['author', 'postType', 'categories'])->all();
+            //dump($posts);
+            return $posts;
+        });
+
+        //return $posts;
+        return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Posts $posts
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Posts $posts)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Posts $posts
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Posts $posts)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Posts $posts
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Posts $posts)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Posts $posts
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Posts $posts)
+    {
+        //
+    }
+}
